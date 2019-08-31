e6ea2f00a282cc87a07fadd70f9b26720ee87c54
Navneet <saluja.navneet2002@gmail.com>
2019-08-31 15:37:04 +0530

    Modified templates to show posts from database.

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    objects = models.Manager
 
     def publish(self):
         self.published_date = timezone.now()
